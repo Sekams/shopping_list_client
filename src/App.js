b1efcbdca3de9526e0edd8f8e81e081fae71f607
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-11-13 18:19:04 +0300

    feat(App): Render all components
    - Render all components to ensure they are working well
    
    [Finishes #151842838]

--- src/App.js
+++ src/App.js
@@ -1,18 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/nav_bar'
+// import LoginFrom from './components/login_form'
+// import RegistrationForm from './components/registration_form'
+// import ShoppingListItem from './components/shopping_list_item'
+// import ShoppingList from './components/shopping_list'
+// import HomePage from './components/home_page'
+import SnackBar from './components/snackbar'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: 'Sekams',
+      authorized: true,
+      logged_in: true,
+      page: 'home'
+    };
+  }
+
   render() {
+    var toiletries = [
+      {
+        name: "Soap",
+        price: "200",
+        status: true
+      },
+      {
+        name: "Deodorant",
+        price: "300",
+        status: false
+      },
+      {
+        name: "Shaver",
+        price: "100",
+        status: false
+      },
+      {
+        name: "Perfume",
+        price: "600",
+        status: true
+      }
+    ]
+    var groceries = [
+      {
+        name: "Tomatoes",
+        price: "20",
+        status: false
+      },
+      {
+        name: "Onions",
+        price: "50",
+        status: true
+      }
+    ]
+    var shoppingLists = [
+      {
+        title: "Toiletries",
+        shoppingListItems: toiletries
+      },
+      {
+        title: "Groceries",
+        shoppingListItems: groceries
+      }
+    ]
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <NavBar
+          username={this.state.username}
+          authorized={this.state.authorized}
+          logged_in={this.state.logged_in}
+          page={this.state.page} />
+        {/* <LoginFrom /> */}
+        {/* <RegistrationForm /> */}
+        {/* <HomePage
+          shoppingLists={shoppingLists} /> */}
+        <SnackBar
+          class="success-snackbar"
+          message="It works" />
       </div>
     );
   }
