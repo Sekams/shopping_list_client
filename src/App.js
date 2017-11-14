3d4c638f12c9b22d14f013a387d265882260a154
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-11-14 18:06:16 +0300

    refactor(App): Remove direct components
    - Remove direct components and add main mapping component
    
    [Finishes #51842838]

--- src/App.js
+++ src/App.js
@@ -1,86 +1,11 @@
-import React, { Component } from 'react';
-import NavBar from './components/nav_bar'
-// import LoginFrom from './components/login_form'
-// import RegistrationForm from './components/registration_form'
-// import ShoppingListItem from './components/shopping_list_item'
-// import ShoppingList from './components/shopping_list'
-// import HomePage from './components/home_page'
-import SnackBar from './components/snackbar'
+import React from 'react';
+import Main from './components/main'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      username: 'Sekams',
-      authorized: true,
-      logged_in: true,
-      page: 'home'
-    };
-  }
-
-  render() {
-    var toiletries = [
-      {
-        name: "Soap",
-        price: "200",
-        status: true
-      },
-      {
-        name: "Deodorant",
-        price: "300",
-        status: false
-      },
-      {
-        name: "Shaver",
-        price: "100",
-        status: false
-      },
-      {
-        name: "Perfume",
-        price: "600",
-        status: true
-      }
-    ]
-    var groceries = [
-      {
-        name: "Tomatoes",
-        price: "20",
-        status: false
-      },
-      {
-        name: "Onions",
-        price: "50",
-        status: true
-      }
-    ]
-    var shoppingLists = [
-      {
-        title: "Toiletries",
-        shoppingListItems: toiletries
-      },
-      {
-        title: "Groceries",
-        shoppingListItems: groceries
-      }
-    ]
-    return (
-      <div>
-        <NavBar
-          username={this.state.username}
-          authorized={this.state.authorized}
-          logged_in={this.state.logged_in}
-          page={this.state.page} />
-        {/* <LoginFrom /> */}
-        {/* <RegistrationForm /> */}
-        {/* <HomePage
-          shoppingLists={shoppingLists} /> */}
-        <SnackBar
-          class="success-snackbar"
-          message="It works" />
-      </div>
-    );
-  }
+const App = () => {
+  localStorage.setItem("baseUrl", "http://127.0.0.1:5000/v1");
+  return (
+    <Main />
+  );
 }
 
 export default App;
