352b1270d466d355a2da717d46a07a966cd8c044
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-11-20 21:00:42 +0300

    chore(navbar_option_item): Add search callback
    - Add search callback function to the NavbarOptionItem component
    
    [Finishes #151842838]

--- src/components/nav_bar_option_item.js
+++ src/components/nav_bar_option_item.js
@@ -1,44 +1,177 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import SearchBar from './search_bar'
+import NavBarDropdown from './nav_bar_dropdown'
+import Modal from './modal'
 
-const NavBarOptionItem = (props) => {
-    if (props.logged_in && props.authorized && props.page === 'home' && props.name === 'search') {
-        return(
-            <li>
-                <SearchBar />
-            </li>
-        );
+class NavBarOptionItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            show_dropdown: false,
+            show_modal: false,
+            dropdown_owner: '',
+            page_limit: localStorage.getItem("pageLimit")
+        };
     }
-    if (props.username && props.name === 'greeting'){
-        return (
-            <li>
-                <h5 className="nav-text">Hi {props.username}</h5>
-            </li>
-        );
+
+    toggleDropDownChangePw = (event) => {
+        event.preventDefault();
+
+        if (this.state.show_dropdown) {
+            this.setState({
+                show_dropdown: false,
+                show_modal: false,
+                dropdown_owner: "change_password"
+            });
+        }
+        else {
+            this.setState({
+                show_dropdown: true,
+                show_modal: false,
+                dropdown_owner: "change_password"
+            });
+        }
     }
-    if (props.logged_in && props.authorized && props.page === 'home' && props.name === 'link') {
-        return(
-            <li>
-                <Link to='/login' className="navbar-link">Logout</Link>
-            </li>
-        );
+
+    toggleDropDownPageLimit = (event) => {
+        event.preventDefault();
+
+        if (this.state.show_dropdown) {
+            this.setState({
+                show_dropdown: false,
+                show_modal: false,
+                dropdown_owner: "page_limit"
+            });
+        }
+        else {
+            this.setState({
+                show_dropdown: true,
+                show_modal: false,
+                dropdown_owner: "page_limit"
+            });
+        }
     }
-    else if (props.page === 'sign_up' && props.name === 'link') {
-        return(
-            <li>
-                <Link to='/login' className="navbar-link">Login</Link>
-            </li>
-        );
+
+    setPageLimit = (event) => {
+        this.setState({
+            page_limit: localStorage.getItem("pageLimit")
+        });
     }
-    else if (props.page === 'login' && props.name === 'link') {
-        return(
-            <li>
-                <Link to='/register' className="navbar-link">Sign Up</Link>
-            </li>
-        );
+
+    showModal = (event) => {
+        event.preventDefault();
+
+        this.setState({
+            show_modal: true,
+            show_dropdown: false
+        });
     }
-    return null;
+
+    handleLogout = (event) => {
+        event.preventDefault();
+
+        if (localStorage.getItem("accessToken")) {
+            fetch(localStorage.getItem("baseUrl") + '/auth/logout', {
+                method: 'POST',
+                headers: {
+                    "Authorization": "Bearer " + localStorage.getItem("accessToken")
+                }
+            })
+                .then((response) => response.json())
+                .then((responseJson) => {
+                    if (responseJson.status && responseJson.status === "success") {
+                        localStorage.setItem("accessToken", "");
+                        localStorage.setItem("username", localStorage.getItem("username"));
+                        localStorage.setItem("loggedIn", false);
+                        localStorage.setItem("message", responseJson.message);
+                        localStorage.setItem("messageType", "success");
+                        window.location = "/login";
+                    } else {
+                        localStorage.setItem("message", responseJson.message);
+                        localStorage.setItem("messageType", "danger");
+                    }
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+        else {
+            localStorage.setItem("message", "Please Login");
+            localStorage.setItem("messageType", "danger");
+            window.location = "/login";
+        }
+    }
+
+    render() {
+        let dropdown, modal = null;
+
+        if (this.state.show_dropdown) {
+            dropdown = <NavBarDropdown
+                owner={this.state.dropdown_owner}
+                showModal={(event) => this.showModal(event)}
+                setPageLimit={(event) => this.setPageLimit(event)} />;
+        }
+
+        if (this.state.show_modal) {
+            modal = <Modal
+                title="Change Password"
+                owner="change_password" />;
+        }
+
+        if (this.props.logged_in && this.props.authorized && this.props.page === 'home' && this.props.name === 'search') {
+            return (
+                <li>
+                    <div className="navbar-input-divs">
+                        <SearchBar
+                            onSearchTermChange={term => { this.props.onSearchTermChange(term) }} />
+                    </div>
+                    <div className="navbar-input-divs navbar-serch-limit">
+                        <h5 className="nav-text"><a href="" className="navbar-link" onClick={this.toggleDropDownPageLimit}>{this.state.page_limit}<b className="caret"></b></a> Lists per page</h5>
+                        {dropdown}
+                    </div>
+                </li>
+            );
+        }
+        // if (this.props.logged_in && this.props.authorized && this.props.page === 'home' && this.props.name === 'search') {
+        //     return (
+        //         <li>
+        //         </li>
+        //     );
+        // }
+        if (this.props.username && this.props.name === 'greeting') {
+            return (
+                <li>
+                    <h5 className="nav-text">Hi <a href="" onClick={this.toggleDropDownChangePw} className="navbar-link" >{this.props.username}<b className="caret"></b></a></h5>
+                    {dropdown}
+                    {modal}
+                </li>
+            );
+        }
+        if (this.props.logged_in && this.props.authorized && this.props.page === 'home' && this.props.name === 'link') {
+            return (
+                <li>
+                    <a href="" onClick={this.handleLogout} className="navbar-link">Logout</a>
+                </li>
+            );
+        }
+        else if (this.props.page === 'sign_up' && this.props.name === 'link') {
+            return (
+                <li>
+                    <Link to='/login' className="navbar-link">Login</Link>
+                </li>
+            );
+        }
+        else if (this.props.page === 'login' && this.props.name === 'link') {
+            return (
+                <li>
+                    <Link to='/register' className="navbar-link">Sign Up</Link>
+                </li>
+            );
+        }
+        return null;
+    }
 }
 
 export default NavBarOptionItem;
