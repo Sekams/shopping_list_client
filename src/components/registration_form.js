7fda65efd8a48622e7cae4b7a039cdc65770d606
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-11-20 21:08:03 +0300

    chore(registration_form): Handle sign up
    - Handle sign up for users
    
    [Finishes #151842838]

--- src/components/registration_form.js
+++ src/components/registration_form.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
 import NavBar from './nav_bar'
 import SnackBar from './snackbar'
 
@@ -7,44 +6,178 @@ class RegistrationForm extends Component {
     constructor(props) {
         super(props);
 
+        //msg_type can be 'danger' or 'success'
         this.state = {
             username: '',
-            authorized: false,
-            logged_in: false,
-            page: 'sign_up'
+            email: '',
+            password: '',
+            confirm_password: '',
+            msg: '',
+            msg_type: ''
         };
     }
 
+    handleUsername(username) {
+        this.setState({
+            username: username,
+            msg: ''
+        });
+    }
+    handlePassword(password) {
+        this.setState({
+            password: password,
+            msg: ''
+        });
+    }
+    handleEmail(email) {
+        this.setState({
+            email: email,
+            msg: ''
+        });
+    }
+    handleConfirmPassword(confirm_password) {
+        this.setState({
+            confirm_password: confirm_password,
+            msg: ''
+        });
+    }
+
+    verifyEmail(email) {
+        if (!(email.includes("@")) || !(email.split("@")[1].includes(".")) || (email.replace("@", "").includes("@")) || email.includes("@.")) {
+            return false;
+        }
+        return true;
+    }
+
+    matchPasswords(password, confirm_password) {
+        if (password === confirm_password) {
+            return true;
+        }
+        return false
+    }
+
+    handleSignUp = (event) => {
+        event.preventDefault();
+
+        let signUpForm = this.state;
+        let formData = new FormData();
+
+        this.clearMessages();
+
+        if (signUpForm.username && signUpForm.password && signUpForm.email && signUpForm.confirm_password) {
+            formData.append('username', signUpForm.username);
+            if (this.verifyEmail(signUpForm.email)) {
+                formData.append('email', signUpForm.email);
+                if (this.matchPasswords(signUpForm.password, signUpForm.confirm_password)) {
+                    formData.append('password', signUpForm.password);
+                    fetch(localStorage.getItem("baseUrl") + '/auth/register', {
+                        method: 'POST',
+                        body: formData
+                    })
+                        .then((response) => response.json())
+                        .then((responseJson) => {
+        
+                            if (responseJson.status && responseJson.status === "success") {
+                                this.setState({
+                                    msg: responseJson.message,
+                                    msg_type: 'success'
+                                });
+                                localStorage.setItem("message", responseJson.message);
+                                localStorage.setItem("messageType", "success");
+                                this.props.history.push('/login');
+                            } else {
+                                this.setState({
+                                    msg: responseJson.message,
+                                    msg_type: 'danger'
+                                });
+                            }
+                        })
+                        .catch((error) => {
+                            console.error(error);
+                        });
+                }
+                else {
+                    this.setState({
+                        msg: 'Passwords don\'t match!',
+                        msg_type: 'danger'
+                    });
+                }
+            }
+            else {
+                this.setState({
+                    msg: 'Invalid email address!',
+                    msg_type: 'danger'
+                });
+            }
+        }
+        else {
+            this.setState({
+                msg: 'Please fill in all the fields',
+                msg_type: 'danger'
+            });
+        }
+    }
+
+    clearMessages() {
+        this.setState({
+            msg: '',
+            msg_type: ''
+        });
+    }
+
     render() {
+        let snackBar = null;
+
+        if (this.state.msg && this.state.msg_type) {
+            snackBar = <SnackBar
+                class={this.state.msg_type + "-snackbar"}
+                message={this.state.msg} />;
+        }
+
         return (
             <div>
                 <NavBar
-                    username={this.state.username}
-                    authorized={this.state.authorized}
-                    logged_in={this.state.logged_in}
-                    page={this.state.page} />
+                    page='sign_up' />
 
                 <div className="register-wrapper">
                     <div className="card register-card drop-shadow">
                         <h3 className="title">Sign Up</h3>
-                        <form id="sign_up_form_id" name="sign_up_form" action="/signup" method="post" className="register-form">
+                        <form onSubmit={this.handleSignUp}>
 
-                            <input name="username" type="text" className="form-input form-control" placeholder="Username" />
+                            <input
+                                value={this.state.username}
+                                type="text"
+                                className="form-input form-control"
+                                onChange={event => this.handleUsername(event.target.value)}
+                                placeholder="Username" />
 
-                            <input name="email" type="email" className="form-input form-control" placeholder="Email" />
+                            <input
+                                value={this.state.email}
+                                type="email"
+                                className="form-input form-control"
+                                onChange={event => this.handleEmail(event.target.value)}
+                                placeholder="Email" />
 
-                            <input name="password" type="password" className="form-input form-control" placeholder="Password" />
+                            <input
+                                value={this.state.password}
+                                type="password"
+                                className="form-input form-control"
+                                onChange={event => this.handlePassword(event.target.value)}
+                                placeholder="Password" />
 
-                            <input name="confirm_password" type="password" className="form-input form-control" placeholder="Confirm Password" />
+                            <input
+                                value={this.state.confirm_password}
+                                type="password"
+                                className="form-input form-control"
+                                onChange={event => this.handleConfirmPassword(event.target.value)}
+                                placeholder="Confirm Password" />
 
-                            <Link to='/login'><button type="button" className="btn btn-form-submit">Finish</button></Link>
+                            <button type="submit" className="btn btn-form-submit">Finish</button>
                         </form>
                     </div>
                 </div>
 
-                <SnackBar
-                    class="success-snackbar"
-                    message="Register Here" />
+                {snackBar}
             </div>
         );
     }
