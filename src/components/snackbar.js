f4f4cec1d61b71259562a6d5c821905bbd5ffc05
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-11-20 21:22:07 +0300

    refactor(snackbar): Refactor snackbar timer
    - Refactor snackbar timer to use react native timer
    
    [Finishes #151842838]

--- src/components/snackbar.js
+++ src/components/snackbar.js
@@ -1,29 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
+const timer = require('react-native-timer');
 
 class SnackBar extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { class: props.class + " show"};
+        this.state = {
+            class: props.class
+        };
+
+        requestAnimationFrame(() => this.showSnackBar())
+    }
+
+
+
+    componentDidMount() {
+        this._mounted = true;
+        localStorage.setItem("message", "");
+        localStorage.setItem("messageType", "");
+    }
+
+    componentWillUnmount() {
+        this._mounted = false;
+        timer.clearTimeout(this);
     }
 
     showSnackBar() {
-        const theSnackBar = this;
-        setTimeout(
-            function() { 
-                theSnackBar.setState({class: theSnackBar.props.class});
-            }, 
-            3000
-        );
+        if (this._mounted) {
+            this.setState({ class: this.props.class + " show" }, () => timer.setTimeout(
+                this, 'hideSnackBar', () => this.setState({ class: this.props.class }), 3000
+            ));
+        }
     }
 
-    render (){
-        this.showSnackBar();
+    render() {
         return (
-            <div 
-            id="snackbar"
-            // Class can be 'danger-snackbar' or 'success-snackbar'
-            className={this.state.class} > 
+            <div
+                id="snackbar"
+                // Class can be 'danger-snackbar' or 'success-snackbar'
+                className={this.state.class} >
                 <p>{this.props.message}</p>
             </div>
         );
