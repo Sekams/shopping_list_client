441ff315ba4e079e0011f6381799a1866e679b45
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-11-20 20:19:10 +0300

    chore(app): Add base URL
    - Add base URL to the App.js file
    
    [Finishes #151842838]

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Main from './components/main'
 
 const App = () => {
-  localStorage.setItem("baseUrl", "http://127.0.0.1:5000/v1");
+  // localStorage.setItem("baseUrl", "http://127.0.0.1:5000/v1");
+  localStorage.setItem("baseUrl", "https://the-real-shopping-list-api.herokuapp.com/v1");
   return (
     <Main />
   );
